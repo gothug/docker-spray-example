8949392dca63619853cd37510bc0b2099d144256
gothug <kojuhovskiy@gmail.com>
2015-01-24 14:48:40 +0300

    first working service prototype (using rutracker for search)

---
+++ src/main/scala/moviesearch/MovieQuery.scala
@@ -0,0 +1,8 @@
+package moviesearch
+
+/**
+ * Created by kojuhovskiy on 21/01/15.
+ */
+class MovieQuery {
+
+}
---
+++ src/main/scala/moviesearch/RutrackerQuery.scala
@@ -0,0 +1,8 @@
+package moviesearch
+
+/**
+ * Created by kojuhovskiy on 22/01/15.
+ */
+class RutrackerQuery {
+
+}
