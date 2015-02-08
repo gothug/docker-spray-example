da7d2c9becd69762e381363e60ac737da896d2f2
gothug <kojuhovskiy@gmail.com>
2015-02-08 22:23:31 +0300

    QueryActor.scala

--- src/main/scala/moviesearch/QueryActor.scala
+++ src/main/scala/moviesearch/QueryActor.scala
@@ -9,28 +9,8 @@ import org.openqa.selenium.firefox.FirefoxDriver
 
 case class Query(query: MovieQuery)
 
-class QueryActor extends Actor {
-  val firefoxDriver: FirefoxDriver = initFirefoxDriver()
-  
+class QueryActor(firefoxDriver: FirefoxDriver) extends Actor {
   override def receive = {
     case Query(query) => sender ! query.doQuery(Some(firefoxDriver))
-//    case Get(counterName) => sender ! counters.getOrElse(counterName, 0)
-//    case Add(counterName, amount) =>
-//      val newAmount = counters.getOrElse(counterName, 0) + amount
-//      counters = counters + (counterName -> newAmount)
-  }
-
-  def initFirefoxDriver(): FirefoxDriver = {
-    val firefoxDriver: FirefoxDriver = new FirefoxDriver
-
-    val url = "http://rutracker.org/forum/index.php"
-
-    firefoxDriver.get(url)
-
-    firefoxDriver.findElementByName("login_username").sendKeys("Greg89754")
-    firefoxDriver.findElementByName("login_password").sendKeys("parol123")
-    firefoxDriver.findElementByName("login").click()
-
-    firefoxDriver
   }
 }
