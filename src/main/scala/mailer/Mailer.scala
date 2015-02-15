b83b5458b99d4abd9ccff1fbceba3fdd218b6eee
gothug <kojuhovskiy@gmail.com>
2015-02-15 14:18:17 +0300

    Mailer.scala: first version of processWatchLists() handling kickass query and hardcoded watchlist

--- src/main/scala/mailer/Mailer.scala
+++ src/main/scala/mailer/Mailer.scala
@@ -1,17 +1,14 @@
 package mailer
 
-import akka.actor.ActorSystem
-import akka.io.IO
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.pattern.ask
+import akka.routing.RoundRobinRouter
 import akka.util.Timeout
-import com.typesafe.scalalogging.slf4j.Logger
 import com.softwaremill.example.JsonSupport._
+import com.typesafe.scalalogging.slf4j.Logger
 import moviesearch.{KickassQuery, MovieQueryResult}
 import org.slf4j.LoggerFactory
-import spray.can.Http
 import spray.client.pipelining._
-import spray.http.ContentTypes._
-import spray.http.HttpMethods._
 import spray.http._
 import spray.httpx.SprayJsonSupport._
 import watchlistparser.{WatchListMovies, _}
@@ -42,19 +39,31 @@ class Mailer(implicit val actorSystem: ActorSystem, implicit val timeout: Timeou
 
     val movieTitles = getMovieTitles
 
-    val pipeline: HttpRequest => Future[MovieQueryResult] = sendReceive ~> unmarshal[MovieQueryResult]
     val url = "http://localhost:8080/search/kickass"
+
+    val processItemActor = actorSystem.actorOf(Props(new ProcessItemActor()).withRouter(RoundRobinRouter(8)), "processitem")
+
+    val moviesList = movieTitles.list //.filter(x => ("london".r findFirstIn x.title).isDefined)
 
     val responses: List[Future[MovieQueryResult]] =
       for {
-        m <- movieTitles.list
-      } yield pipeline(Post(url, KickassQuery(m.title, None, m.year)))
+        m <- moviesList
+      } yield (processItemActor ? Item(url, m))(500 seconds).mapTo[MovieQueryResult]
 
-    val results: List[MovieQueryResult] = Await.result(Future.sequence(responses), 65 seconds)
+    val results: List[MovieQueryResult] = Await.result(Future.sequence(responses), 6500 seconds)
 
-//    sendMail(r.html)
+    logger.info("Finished processing..")
 
-    val d = 1
+    def toHtml(title: String, year: Int, link: String) = {
+      s"""<p>Title: $title, Year: $year<br><a href="$link">$link</a></p>""".stripMargin
+    }
+
+    val htmls =
+      for {
+        (WatchListParsedMovie(title, year), link) <- moviesList zip results.map(_.link)
+      } yield toHtml(title, year, link)
+
+    sendMail(htmls.mkString("<br>"))
   }
 
   def sendMail(html: String) = {
@@ -65,34 +74,62 @@ class Mailer(implicit val actorSystem: ActorSystem, implicit val timeout: Timeou
 
     val url = proto + domain + basepath + path
 
-    val emailJson =
-      s"""{
-          "key" : "BAXsOmtwxAELZKVQWohlzQ",
-          "message" : {
-            "html" : "$html",
-            "text" : "Example text content",
-            "subject" : "example subject",
-            "from_email" : "kojuhovskiy@gmail.com",
-            "from_name" : "Example Name",
-            "to" : [
-              {
-                "email" : "kojuhovskiy@gmail.com",
-                "name" : "Vasek",
-                "type" : "to"
-              }
-            ],
-            "headers" : {
-              "Reply-To" : "kojuhovskiy@gmail.com"
-            }
-          }
-        }"""
+    val htmlEscaped = html.replace(""""""", """\"""")
 
-    val response: Future[HttpResponse] =
-      (IO(Http) ?
-        HttpRequest(method = POST, uri = Uri(url),
-          entity = HttpEntity(`application/json`, emailJson))).mapTo[HttpResponse]
+    val emailJson =
+      s"""|{
+          |   "key" : "BAXsOmtwxAELZKVQWohlzQ",
+          |   "message" : {
+          |     "html" : "$htmlEscaped",
+          |     "text" : "http://google.com",
+          |     "subject" : "example subject",
+          |     "from_email" : "kojuhovskiy@gmail.com",
+          |     "from_name" : "Example Name",
+          |     "to" : [
+          |       {
+          |         "email" : "kojuhovskiy@gmail.com",
+          |         "name" : "Vasek",
+          |         "type" : "to"
+          |       }
+          |     ],
+          |     "headers" : {
+          |       "Reply-To" : "kojuhovskiy@gmail.com"
+          |     },
+          |     "auto_html": true
+          |   }
+          |}""".stripMargin
+
+    val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
+    val response: Future[HttpResponse] = pipeline(Post(url, emailJson))
 
     logger.info("Sending e-mail..")
+
+    val r = Await.result(response, 65 seconds)
+
+    if (r.status.toString == "200 OK") {
+      logger.info("Email sent successfully")
+    } else {
+      logger.info("Problems with sending email")
+      logger.info(r.entity.toString)
+    }
+
+    val d = 1
+  }
+}
+
+case class Item(url: String, query: WatchListParsedMovie)
+
+class ProcessItemActor() extends Actor {
+  import context.dispatcher
+
+  override def receive = {
+    case Item(url, wlMovie) => sender ! processRequest(url, wlMovie)
+  }
+
+  def processRequest(url: String, movie: WatchListParsedMovie): MovieQueryResult = {
+    val pipeline: HttpRequest => Future[MovieQueryResult] = sendReceive ~> unmarshal[MovieQueryResult]
+    val response: Future[MovieQueryResult] = pipeline(Post(url, KickassQuery(movie.title, None, movie.year)))
+    Await.result(response, 30 seconds)
   }
 }
 
